f856c4a4d1fcdfec7f2dc651c278fa4fefb8634a
Bora Kim <stellakkkk@hotmail.com>
2020-12-22 11:01:39 +0000

    Add button function enhanced

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const salary = document.getElementById('salary1');
 let hourlyRate;
 let date = document.getElementById('date');
 let hours = document.getElementById('hours');
+let i =0;
 const data = {
     date : [],
     hours : []
@@ -16,9 +17,17 @@ document.querySelector(".salary").addEventListener("click", function(){
     hourlyRate = hourlyRate / 12 / 161;
     console.log(hourlyRate)
     document.querySelector("p").insertAdjacentHTML('beforeend', hourlyRate);
+    const disabled = document.querySelectorAll(".disabled");
+    for (let i = 0; i < disabled.length; i++){
+        disabled[i].removeAttribute("disabled")
+    }
 })
+
 
 document.querySelector(".Add").addEventListener("click", function(){
     data.date.push(date.value);
-    data.hours.push(hours.value);    
+    data.hours.push(hours.value);  
+    const addHTML = `<li><strong>${data.date[i]}</strong> You have worked ${data.hours[i]} hours</li><button>Edit</button><button>Update</button><button>Delete</button>`
+    document.querySelector('ul').insertAdjacentHTML('afterbegin', addHTML);
+    i++;
 })
