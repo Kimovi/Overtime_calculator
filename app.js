e053ecc53e82756fca4cab2bb1000e106471736e
Bora Kim <stellakkkk@hotmail.com>
2021-01-15 19:11:01 +0000

    Fixed variables name to avoid confusion

--- app.js
+++ app.js
@@ -2,10 +2,10 @@ const salary = document.getElementById('salary1');
 let hourlyRate;
 let date = document.getElementById('date');
 let hours = document.getElementById('hours');
-let i =0;
+let i =0; // This will be pushed to data.counter to use it as id
 const data = {
-    date : [],
-    hours : [],
+    dateArr : [],
+    hoursArr : [],
     counter : []
 }
 
@@ -14,7 +14,7 @@ document.querySelector('.salary').addEventListener('click', function(){
     hourlyRate = Math.floor((hourlyRate / 12 / 161) * 1.5);
     const disabled = document.querySelectorAll('.disabled');
     for (let i = 0; i < disabled.length; i++){
-        disabled[i].removeAttribute('disabled')
+        disabled[i].removeAttribute('disabled') //When 'submit' button clicked, two input box on the bottom will be enabled
     }
     //Enable text in 'p' element
     document.getElementById('hourlyRate').style.display = 'block';    
@@ -22,14 +22,14 @@ document.querySelector('.salary').addEventListener('click', function(){
 })
 
 document.querySelector('.Add').addEventListener('click', function(){
-    data.date.push(date.value);
-    data.hours.push(hours.value);  
-    const addHTML = `<div id='${i}'><li><strong>${data.date[i]}</strong> You have worked ${data.hours[i]} hours</li><button class='edit'>Edit</button><button class='update'>Update</button><button class='delete'>Delete</button></div>`
+    data.dateArr.push(date.value);
+    data.hoursArr.push(hours.value);  
+    const addHTML = `<div id='${i}'><li><strong>${data.dateArr[i]}</strong> You have worked ${data.hoursArr[i]} hours</li><button class='edit'>Edit</button><button class='update'>Update</button><button class='delete'>Delete</button></div>`
     document.querySelector('ul').insertAdjacentHTML('afterbegin', addHTML);
     data.counter.push(i);
     i++;
 
-    const sumHours = sum(data.hours);
+    const sumHours = sum(data.hoursArr);
     document.getElementById('total').innerHTML=`You've worked <strong>${sumHours}</strong> hours and this would be <strong>£${sumHours * hourlyRate}</strong>`;
 
     //Enable text in 'p' element
@@ -53,8 +53,8 @@ document.getElementById('list').addEventListener('click', function(e) {
     const targetID = tgt.parentNode.id;
 
     if(tgt.classList.contains('edit')){
-        date.value = data.date[targetID];
-        hours.value = data.hours[targetID];
+        date.value = data.dateArr[targetID];
+        hours.value = data.hoursArr[targetID];
         date.style.color = 'rgb(236, 141, 32)';
         hours.style.color = 'rgb(236, 141, 32)';
         date.style.borderColor = 'rgb(236, 141, 32)';
@@ -63,10 +63,10 @@ document.getElementById('list').addEventListener('click', function(e) {
     } else if(tgt.classList.contains('update')){
         console.log('update')
         // console.log(document.getElementById('date').value, document.getElementById('hours').value);
-        data.date.splice(targetID, 1, date.value);
-        data.hours.splice(targetID, 1, hours.value);
+        data.dateArr.splice(targetID, 1, date.value);
+        data.hoursArr.splice(targetID, 1, hours.value);
         let currentNode = tgt.parentNode.childNodes[0];
-        currentNode.innerHTML=`<div id='${targetID}'><li><strong>${data.date[targetID]}</strong> You have worked ${data.hours[targetID]} hours</li>`;
+        currentNode.innerHTML=`<div id='${targetID}'><li><strong>${data.dateArr[targetID]}</strong> You have worked ${data.hoursArr[targetID]} hours</li>`;
 
         //disable the style when 'update' completed
         date.style.color = '';
@@ -74,7 +74,7 @@ document.getElementById('list').addEventListener('click', function(e) {
         date.style.borderColor = '';
         hours.style.borderColor = '';
 
-        const sumHours = sum(data.hours);
+        const sumHours = sum(data.hoursArr);
         document.getElementById('total').innerHTML=`You've worked <strong>${sumHours}</strong> hours and this would be <strong>£${sumHours * hourlyRate}</strong>`;
     
         clear();
@@ -83,8 +83,8 @@ document.getElementById('list').addEventListener('click', function(e) {
         //remove HTML tag contains 'delete' element
         tgt.parentNode.remove();
         //remove data from 'data' object
-        data.date.splice(targetID, 1)
-        data.hours.splice(targetID, 1)
+        data.dateArr.splice(targetID, 1)
+        data.hoursArr.splice(targetID, 1)
         data.counter.splice(targetID, 1)
     }
 });
